3199d3d052011546d3d87066c08d7fdde5e4126f
Josep M. Badia <josepmaria.badia@urv.cat>
2021-10-02 10:18:01 +0200

    docs: No create help page

--- R/customValues.R
+++ R/customValues.R
@@ -1,5 +1,6 @@
 #' Vector of custom colors
 #'
+#'@noRd
 .MS2IDcols = c(
     qry = '#00786C',
     ref = '#b40000',
@@ -10,6 +11,7 @@
 #'
 #' palette for spectra that forms consensus (MS2IDgui, CONS plot)
 #'
+#' @noRd
 .colCons = c("#FF0000", "#00A08A", "#F2AD00", "#5BBCD6", "#972D15",
             "#ECCBAE", "#046C9A", "#D69C4E", "#ABDDDE", "#000000",
             "#D8B70A", "#02401B", "#A2A475", "#81A88D", "#F98400"
