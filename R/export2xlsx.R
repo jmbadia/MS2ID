b70d6ede7c33cd91ecc2e11d2b01d0cc52d9f2d7
Josep M. Badia <josepmaria.badia@urv.cat>
2021-10-27 19:48:19 +0200

    feat: arguments and others

--- R/export2xlsx.R
+++ R/export2xlsx.R
@@ -1,21 +1,74 @@
-#' Export an annotation object as xlsx file
+#' @title Export an annotation object to an xlsx file
 #'
-#' @param anRslt Annot object with the results to be exported
-#' @param noCmnNeutralMassDist numeric(1) with a distance threshold to subset Non common neutral mass results.
-#' @param metric char(1) with the name of the metric that must subset (according to the distance threshold) and order the identifications.
-#' @param file char(1) name (with path) of the exported file
+#' @description \code{export2xlsx} function exports the data contained in an
+#' \linkS4class{Annot} object to a xlsx file.
+#'
+#' @details
+#' \code{export2xlsx} function distributes the annotation results among
+#' different worksheets of an xlsx file, one per query file, and every row
+#' corresponds to an annotation. Annotations of the same query spectrum have
+#' identical row colours. Contiguous annotations with equal query precursor mass
+#' have similar row colours (e.g. variations of orange); the best distance
+#' metric and the most common compound name among those contiguous annotations
+#' are bolded in red and black. Moreover:
+#' \itemize{\item Column names that contain QRY, REF or CONS refer to query,
+#' reference or consensus entities, respectively. e.g. \code{QRYprecursorMz}
+#' contains the precursor M/z of the query spectra, \code{REFadduct} the adducts
+#' of the reference spectra and \code{QRYrtime_CONS} the retention times of the
+#' query spectra that conform every consensus spectrum. \item The column
+#' \code{massNum.QRY_CMN_REF} (e.g. 7/4/5) shows the number of fragments of the
+#' query and reference spectra, and the number of fragments they have in common.
+#' \item \code{REFinchikey} contains the inchikey of the reference compound and
+#' a link to a Pubchem's page listing compounds with that inchikey. \item
+#' \code{ppmPrecMass} refers to the difference, in ppm, between query and
+#' reference precursor masses.}
+#'
+#' @param anRslt \linkS4class{Annot} object with the results to export.
+#' @param summarizeHits boolean(1). A TRUE value summarizes the resulting excel,
+#'   keeping only, for every query spectrum, the best annotation per compound.
+#' @param file char(1) name of the resulting xlsx file
 #' @param overwrite boolean(1) If TRUE, overwrite any existing file.
+#' @param ... other arguments passed to function
+#' @author Josep M. Badia \email{josepmaria.badia@@urv.cat}
+#' @seealso \linkS4class{Annot} class and \code{\link{annotate}} function.
+#' @example man/examples/loadMS2ID.R
+#' @example man/examples/selectQuerySpectra.R
+#' @examples
+#'
+#' ## ANNOTATION ---
+#' library(MS2ID)
+#' MS2IDlib <- MS2ID(MS2IDFolder)
+#' annotResult <- annotate(QRYdata = queryFolder, MS2ID = MS2IDlib)
+#'
+#' ## EXPORT TO XLSX FILE---
+#' export2xlsx(anRslt = annotResult, file = "sample",
+#'             summarizeHits = FALSE, overwrite=TRUE)
 #' @export
-export2xlsx <- function(anRslt, summarizeHits, ...){
-    #TODO CHECK ARGUMENTS
-    anRslt <- .export2df(anRslt, summarizeHits = summarizeHits, ...)
-
+export2xlsx <- function(anRslt, summarizeHits = TRUE, file = NULL,
+                        overwrite = FALSE, ...){
+    argmnts <- c(as.list(environment()), list(...))
+    reqClasses <- c(anRslt = "Annot", summarizeHits = "logical",
+                    file = "character", overwrite = "logical")
+    reqClasses <- reqClasses[names(reqClasses) %in% names(argmnts)]
+    .checkTypes(argmnts[match(names(reqClasses), names(argmnts))], reqClasses)
+    if(missing(anRslt))
+            stop("'anRslt' argument is mandatory")
+    dfRslt <- .export2df(anRslt, summarizeHits = summarizeHits, ...)
     #Save xlsx file
-    .create_xlsx(data = anRslt, ...)
+    .create_xlsx(data = dfRslt, file = file, overwrite = overwrite, ...)
 }
 
 #' @importFrom openxlsx createStyle createWorkbook addWorksheet writeData addStyle writeFormula
-.create_xlsx <- function(data, metric="cosine", ...){
+.create_xlsx <- function(data = NULL, metric = "cosine", file = NULL, ...){
+    argmnts <- c(as.list(environment()), list(...))
+    reqClasses <- c(data = "dataframe", metric = "character",
+                    file="character")
+    reqClasses <- reqClasses[names(reqClasses) %in% names(argmnts)]
+    .checkTypes(argmnts[match(names(reqClasses), names(argmnts))], reqClasses)
+    if(is.null(data))
+        stop("'data' argument is mandatory")
+    if(is.null(file))
+        stop("'file' argument is mandatory")
     if(length(metric)!=1){
         stop("'metric' must contain ONE string")
     }else if(!(metric %in% c(INCRMETRIC, DECRMETRIC))){
@@ -25,10 +78,10 @@ export2xlsx <- function(anRslt, summarizeHits, ...){
 
     #collpse "QRYmassNum","cmnMasses","REFmassNum" in just one column
     massNum_var <- c("QRYmassNum","cmnMasses","REFmassNum")
-    data$QRY_CMN_REF_massNum <- vapply(seq_len(nrow(data)), function(nr){
+    data$massNum.QRY_CMN_REF <- vapply(seq_len(nrow(data)), function(nr){
         paste(data[nr, massNum_var], collapse = "/")
     }, FUN.VALUE = "rita")
-    data <- dplyr::relocate(data, QRY_CMN_REF_massNum, .before=QRYmassNum)
+    data <- dplyr::relocate(data, massNum.QRY_CMN_REF, .before = QRYmassNum)
     data <- dplyr::select(data, !massNum_var)
 
     #remove empty columns
@@ -51,7 +104,7 @@ export2xlsx <- function(anRslt, summarizeHits, ...){
 
     ## style for body
     bodyStyle <- createStyle(border="TopBottom", borderColour = "#4F81BD")
-    m <- which(diff(data$idQRYspect)!=0)
+    # m <- which(diff(data$idQRYspect) != 0)
 
     #Every precursor mass has alternate color
     StyleA1 <- createStyle(border="TopBottom", fgFill = "#ff9a3c")
@@ -68,10 +121,9 @@ export2xlsx <- function(anRslt, summarizeHits, ...){
     wb <- createWorkbook(creator = "jmbadia", title="My name here")
     for(idFile in unique(data$QRYdataOrigin)) {
         #select data
-        tmp <- data[data$QRYdataOrigin==idFile,]
+        tmp <- data[data$QRYdataOrigin == idFile,]
         #only first 30 characters of the filename
         idFile <- substr(idFile, start = 1, stop = min(30,nchar(idFile)))
-
         #for every precMass, which has the best cossim
         bst_cossim <- unlist(lapply(unique(tmp$QRYprecursorMz), function(x) {
             maxValue <- max(tmp[tmp$QRYprecursorMz==x, cossimCol])
@@ -121,20 +173,20 @@ export2xlsx <- function(anRslt, summarizeHits, ...){
                  stack = TRUE)
 
         #add link
-        noInchikey <- is.na(tmp$REFinchikey)
-        nameLinks <- unlist(tmp$REFinchikey[!noInchikey])
+        cellInch <- tmp$REFinchikey
+        noInch <- is.na(cellInch)
+        names(cellInch) <- cellInch
+        names(cellInch)[noInch] <- ""
+        cellInch[noInch] <- ""
         baselink <- 'https://www.ncbi.nlm.nih.gov/pccompound?term=%22'
-        tmp$REFinchikey[!noInchikey] <- paste0(baselink,
-                                               tmp$REFinchikey[!noInchikey],
-                                               '%22[InChIKey]')
-        xx <- unlist(tmp$REFinchikey)
-        names(xx)[noInchikey] <- "NA"
-        names(xx)[!noInchikey] <- nameLinks
-        names(xx) <- rep("dd",length(xx))
-        class(xx) <- "hyperlink"
-        writeData(wb, sheet = idFile, x = xx, startRow = 2, startCol = inchikeyCol)
-        #writeFormula(wb, sheet =idFile, startRow = 2, startCol = inchikeyCol, x = tmp$REFinchikey)
-
+        cellInch[!noInch] <- paste0(baselink, cellInch[!noInch],
+                                    '%22[InChIKey]')
+        class(cellInch) <- "hyperlink"
+        openxlsx::writeData(wb, sheet = idFile, x = cellInch, startRow = 2,
+                  startCol = inchikeyCol)
     }
-    openxlsx::saveWorkbook(wb, ...)
+    #add xlsx extension if missing
+    ex <- strsplit(basename(file), split="\\.")[[1]]
+    if(!identical(ex[-1], "xlsx")) file <- paste0(file, ".xlsx")
+    openxlsx::saveWorkbook(wb, file = file, ...)
 }
