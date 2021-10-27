0feb0b769af99a478daf2bf8896eb7403662d13c
Josep M. Badia <josepmaria.badia@urv.cat>
2021-10-27 19:12:45 +0200

    feat: show method for Annot object

--- R/Annot-methods.R
+++ R/Annot-methods.R
@@ -1,21 +1,58 @@
 #' @include Annot.R
 
-#' @export
-#' @describeIn Annot Getter for the refCompound slot
+#' Subset method for Annot class
+#'
+#' @param x character: signature supported
+#' @rdname Annot
+#' @exportMethod refCompound
 setMethod("refCompound", "Annot", function(x) x@refCompound)
 
-#' @export
-#' @describeIn Annot Getter for the qrySpectra slot
+#' Subset method for Annot class
+#'
+#' @param x character: signature supported
+#' @rdname Annot
+#' @exportMethod qrySpectra
 setMethod("qrySpectra", "Annot", function(x) x@qrySpectra)
 
-#' @export
-#' @describeIn Annot Getter for the refSpectra slot
+#' Subset method for Annot class
+#'
+#' @param x character: signature supported
+#' @rdname Annot
+#' @exportMethod refSpectra
 setMethod("refSpectra", "Annot", function(x) x@refSpectra)
 
-#' @export
-#' @describeIn Annot Getter for the hits slot
+#' Subset method for Annot class
+#'
+#' @param x character: signature supported
+#' @rdname Annot
+#' @exportMethod hits
 setMethod("hits", "Annot", function(x) x@hits)
 
-#' @export
-#' @describeIn Annot Getter for the infoAnnotation slot
+#' Subset method for Annot class
+#'
+#' @param x character: signature supported
+#' @rdname Annot
+#' @exportMethod infoAnnotation
 setMethod("infoAnnotation", "Annot", function(x) x@infoAnnotation)
+
+#' Subset method for Annot class
+#'
+#' @param object character: signature supported
+#' @rdname Annot
+#' @importMethodsFrom methods show
+#' @exportMethod show
+setMethod("show", "Annot",
+          function(object) {
+              hits <- hits(object)
+              cat("Annot object with annotation results: \nContains ",
+                  length(unique(hits$idQRYspect)),
+                  " different query spectra annotated with ",
+                  length(unique(hits$idREFcomp)),
+                  " different compounds\n", sep = "")
+              info <- object@infoAnnotation
+              cat("Database original: ", info$MS2ID,
+                  "\nQuery spectra source: ", info$QRYdata,
+                  "\nAnnotation date: ", info$annotationTime,
+                  "\nUse hits() for more parameters used in the annotation.",
+                  "\n", sep = "")
+          })
